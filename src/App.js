62ef49db0c84dc9e0f8bbe34a6954a6862ab6bdb
Luca A. Müller <aired@posteo.hu>
2023-02-26 21:02:29 +0000

    Add unknown word warning animation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
+  const [words, setWords] = useState([])
   const [solution, setSolution] = useState('')
-  // this is very stup as you can use every key only once
   const [guesses, addGuess] = useState([])
   const [input, setInput] = useState('')
-
+  const [inputCSSclass, setInputCSSclass] = useState('')
   // howto use useEffect
   // Object.entries etc.
   // how to check if substring
@@ -14,6 +14,7 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       const words = ["ALBUM","HINGE","MONEY","SCRAP","GAMER","GLASS","SCOUR","BEING","DELVE","YIELD","METAL","TIPSY","SLUNG","FARCE","GECKO","SHINE","CANNY","MIDST","BADGE","HOMER","TRAIN","STORY","HAIRY","FORGO","LARVA","TRASH","ZESTY","SHOWN","HEIST","ASKEW","INERT","OLIVE","PLANT","OXIDE","CARGO","FOYER","FLAIR","AMPLE","CHEEK","SHAME","MINCE","CHUNK","ROYAL","SQUAD","BLACK","STAIR","SCARE","FORAY","COMMA","NATAL","SHAWL","FEWER","TROPE","SNOUT","LOWLY","STOVE","SHALL","FOUND","NYMPH","EPOXY","DEPOT","CHEST","PURGE","SLOSH","THEIR","RENEW","ALLOW","SAUTE","MOVIE","CATER","TEASE","SMELT","FOCUS","TODAY","WATCH","LAPSE","MONTH","SWEET","HOARD","CLOTH","BRINE","AHEAD","MOURN","NASTY","RUPEE","CHOKE","CHANT","SPILL","VIVID","BLOKE","TROVE","THORN","OTHER","TACIT","SWILL","DODGE","SHAKE","CAULK","AROMA","CYNIC","ROBIN","ULTRA","ULCER","PAUSE","HUMOR","FRAME","ELDER","SKILL","ALOFT","PLEAT","SHARD","MOIST","THOSE","LIGHT","WRUNG","COULD","PERKY","MOUNT","WHACK","SUGAR","KNOLL","CRIMP","WINCE","PRICK","ROBOT","POINT","PROXY","SHIRE","SOLAR","PANIC","TANGY","ABBEY","FAVOR","DRINK","QUERY","GORGE","CRANK","SLUMP","BANAL","TIGER","SIEGE","TRUSS","BOOST","REBUS","UNIFY","TROLL","TAPIR","ASIDE","FERRY","ACUTE","PICKY","WEARY","GRIPE","CRAZE","PLUCK","BRAKE","BATON","CHAMP","PEACH","USING","TRACE","VITAL","SONIC","MASSE","CONIC","VIRAL","RHINO","BREAK","TRIAD","EPOCH","USHER","EXULT","GRIME","CHEAT","SOLVE","BRING","PROVE","STORE","TILDE","CLOCK","WROTE","RETCH","PERCH","ROUGE","RADIO","SURER","FINER","VODKA","HERON","CHILL","GAUDY","PITHY","SMART","BADLY","ROGUE","GROUP","FIXER","GROIN","DUCHY","COAST","BLURT","PULPY","ALTAR","GREAT","BRIAR","CLICK","GOUGE","WORLD","ERODE","BOOZY","DOZEN","FLING","GROWL","ABYSS","STEED","ENEMA","JAUNT","COMET","TWEED","PILOT","DUTCH","BELCH","OUGHT","DOWRY","THUMB","HYPER","HATCH","ALONE","MOTOR","ABACK","GUILD","KEBAB","SPEND","FJORD","ESSAY","SPRAY","SPICY","AGATE","SALAD","BASIC","MOULT","CORNY","FORGE","CIVIC","ISLET","LABOR","GAMMA","LYING","AUDIT","ROUND","LOOPY","LUSTY","GOLEM","GONER","GREET","START","LAPEL","BIOME","PARRY","SHRUB","FRONT","WOOER","TOTEM","FLICK","DELTA","BLEED","ARGUE","SWIRL","ERROR","AGREE","OFFAL","FLUME","CRASS","PANEL","STOUT","BRIBE","DRAIN","YEARN","PRINT","SEEDY","IVORY","BELLY","STAND","FIRST","FORTH","BOOBY","FLESH","UNMET","LINEN","MAXIM","POUND","MIMIC","SPIKE","CLUCK","CRATE","DIGIT","REPAY","SOWER","CRAZY","ADOBE","OUTDO","TRAWL","WHELP","UNFED","PAPER","STAFF","CROAK","HELIX","FLOSS","PRIDE","BATTY","REACT","MARRY","ABASE","COLON","STOOL","CRUST","FRESH","DEATH","MAJOR","FEIGN","ABATE","BENCH","QUIET","GRADE","STINK","KARMA","MODEL","DWARF","HEATH","SERVE","NAVAL","EVADE","FOCAL","BLUSH","AWAKE","HUMPH","SISSY","REBUT","CIGAR"]
+      setWords(words)
       const solution = words[Math.floor(Math.random()*words.length)]
       setSolution(solution)
       console.log(solution)
@@ -27,19 +28,30 @@ function App() {
 
   const handleInput = (new_input) => {
     if (new_input.length === 5) {
-      let guess = new Array(5)
-      for (let i = 0; i < new_input.length; i++) {
-        const letter = new_input[i]
-        const color = new_input[i] === solution[i] ? '#6aaa64' 
-                      : solution.includes(new_input[i]) ? '#c9b458' 
-                      : 'grey'
-        guess[i] = [letter, color]
+      if (words.includes(new_input)) {
+        let guess = new Array(5)
+        for (let i = 0; i < new_input.length; i++) {
+          const letter = new_input[i]
+          const color = new_input[i] === solution[i] ? '#6aaa64' 
+                        : solution.includes(new_input[i]) ? '#c9b458' 
+                        : 'grey'
+          guess[i] = [letter, color]
+        }
+        addGuess([...guesses, guess])
+      } else {
+        handleUnknownWord()
       }
-      addGuess([...guesses, guess])
       setInput('')
     } else {
       setInput(new_input)
     }
+  }
+
+  const handleUnknownWord = () => {
+    setInputCSSclass('guess-input')
+    setTimeout(() => {
+      setInputCSSclass('')
+    }, 500)
   }
 
   return (
@@ -93,7 +105,8 @@ function App() {
       )}</div>
       <div style={{display: 'flex', justifyContent: 'center'}}>
         {guesses.length < 6 ? 
-        <div>Guess:{' '} 
+        <div className={inputCSSclass}>
+          Guess:{' '} 
           <input
             value={input}
             onChange={e => handleInput(e.target.value)} 
