31b71863a9bf3abea469f165da3180739dce9a12
Luca A. Müller <aired@posteo.hu>
2023-04-10 19:08:09 +0000

    Add some TODOs

--- src/App.js
+++ src/App.js
@@ -128,6 +128,7 @@ function App() {
   /** TODO: coding
    * - refactor the code so that there is only one toast component
    * - add a timer to the toast component, showing it for 5000ms
+   * - add a listener to keydown that passes the uppercase of a letter input to the letterButtonHandler
    * 
 
   /* TODO: styling
